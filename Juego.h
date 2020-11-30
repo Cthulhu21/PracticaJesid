762ee6ff2a1f14d9fd308ef7166d3c43503183d3
Cthulhu21 <62732060+Cthulhu21@users.noreply.github.com>
2020-11-30 17:33:00 -0500

    Tamaños y Escenario
    Se ponen tamaños a la pantalla y se agrega el escenario

--- Juego.h
+++ Juego.h
@@ -2,11 +2,13 @@
 #define JUEGO_H
 
 #include <QGraphicsView>
+#include <QGraphicsScene>
 
 class Juego : public QGraphicsView
 {
     Q_OBJECT
 public:
     Juego(QWidget *parent = nullptr);
+    QGraphicsScene *Escenario;
 };
 #endif // JUEGO_H
--- main.cpp
+++ main.cpp
@@ -8,6 +8,5 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     Game = new Juego;
     Game->show();
-    int B;
     return a.exec();
 }
