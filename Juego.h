6cacddcd181f04954b12cecdd7ea369dfd3dbd20
Cthulhu21 <62732060+Cthulhu21@users.noreply.github.com>
2020-11-30 16:38:04 -0500

    Inicial

---
+++ Juego.h
@@ -0,0 +1,14 @@
+#ifndef JUEGO_H
+#define JUEGO_H
+
+#include <QMainWindow>
+
+class Juego : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    Juego(QWidget *parent = nullptr);
+    ~Juego();
+};
+#endif // JUEGO_H
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "Juego.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Juego w;
+    w.show();
+    return a.exec();
+}
