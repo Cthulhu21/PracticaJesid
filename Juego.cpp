fc80f364beb8a91ccd3622f932826b3b6d6659b4
Tokanimo <jesid.lopez@udea.edu.co>
2020-11-30 17:33:33 -0500

    Escenario
    Se crea y establecen las dimensiones del escenario

--- Juego.cpp
+++ Juego.cpp
@@ -1,7 +1,16 @@
 #include "Juego.h"
+#include <windows.h>
 
 Juego::Juego(QWidget *parent)
 {
+    int Ancho = GetSystemMetrics(SM_CXSCREEN);
+    int Alto = GetSystemMetrics(SM_CYSCREEN);
+    setFixedSize(Ancho,Alto);
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    Escenario = new QGraphicsScene;
+    Escenario->setSceneRect(0,0,Ancho,Alto);
+    setScene(Escenario);
 }
 
 
